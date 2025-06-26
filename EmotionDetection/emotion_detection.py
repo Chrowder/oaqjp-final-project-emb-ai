538562927d1b9340aff6b290b22317a972174c7e
Chrowder <109297967+Chrowder@users.noreply.github.com>
2025-06-26 00:57:27 -0400

    Task7 finished

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,32 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } } 
     response = requests.post(URL, json = myobj, headers=Headers) 
 
+
     formatted_response = json.loads(response.text)
-    emotion = formatted_response['emotionPredictions'][0]
+    
     result = dict()
+    if response.status_code == 200:
+        emotion = formatted_response['emotionPredictions'][0]
+        result["anger"] = emotion['emotion']['anger']
+        result["disgust"] = emotion['emotion']['disgust']
+        result["fear"] = emotion['emotion']['fear']
+        result["joy"] = emotion['emotion']['joy']
+        result["sadness"] = emotion['emotion']['sadness']
 
-    result["anger"] = emotion['emotion']['anger']
-    result["disgust"] = emotion['emotion']['disgust']
-    result["fear"] = emotion['emotion']['fear']
-    result["joy"] = emotion['emotion']['joy']
-    result["sadness"] = emotion['emotion']['sadness']
+        max = result["anger"]
+        dominant_emotion = "anger"
+        for key, value in result.items():
+            if value > max:
+                max = value
+                dominant_emotion = key
+        result["dominant_emotion"] = dominant_emotion
 
-    max = result["anger"]
-    dominant_emotion = "anger"
-    for key, value in result.items():
-        if value > max:
-            max = value
-            dominant_emotion = key
-    result["dominant_emotion"] = dominant_emotion
+        return result
 
+    result["anger"] = None
+    result["disgust"] = None
+    result["fear"] = None
+    result["joy"] = None
+    result["sadness"] = None
+    result["dominant_emotion"] = None
     return  result
--- server.py
+++ server.py
@@ -13,6 +13,9 @@ def render_index_page():
 def emotion_detect():
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
+
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
     
     str = f"""For the given statement, the system response is 'anger': {result['anger']}, 
     'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy':{result['joy']} 
